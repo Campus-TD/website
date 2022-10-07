7d540e2de88932bd2c77acda59f7656ed5b5a77b
Josephglz <joseph220199@gmail.com>
2022-10-06 22:22:24 -0500

    styles, content, and new plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './src/**/*.{html,js}',
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#21327a",
+        whiteText: "#DEE2E6",
+        logo: "#4285f4",
+      },
+    },
     fontFamily: {
       quicksand: ['Quicksand'],
     },
-    colors: {
-      background: "#21327a",
-      whiteText: "#DEE2E6",
-      logo: "#4285f4",
-    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar')
+  ],
 }
